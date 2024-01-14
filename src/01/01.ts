8cca6586a55c329e23f06295fe2c3127ac76099c
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2024-01-14 15:40:09 +0100

    add comments

--- src/01/01.ts
+++ src/01/01.ts
@@ -7,7 +7,7 @@ const filePath = './input1.txt';
 try {
   const filePath = './input1.txt';
 
-  fileContent = fs.readFileSync(filePath, 'utf-8');
+  fileContent = fs.readFileSync(filePath, 'utf-8');//read input file
 
 } catch (err) {
   console.error('Error reading the file:', err);
@@ -15,14 +15,14 @@ try {
 
 
 
-const lines = fileContent?.split('\n');
+const lines = fileContent?.split('\n');//split input files into arrays of 
 let sumOfLine : number = 0;
-lines?.forEach((line, index) => {
+lines?.forEach((line, index) => {//Iterate over the lines
   let actualLine = line;
-  let arr = actualLine.split('');
-  
-  let firstNumber : number | undefined;
+  let arr = actualLine.split('');//split actual line into a chars of arrays
+  let firstNumber : number | undefined; 
   let lastNumber : number | undefined;
+  //iterate over the line from the begining and find first number
   for(const word of arr) {
     const parsedNumber = parseInt(word);
   if (!isNaN(parsedNumber)) {
@@ -30,7 +30,7 @@ lines?.forEach((line, index) => {
       break;
     }
   }
-
+  //iterate from the end and find the last number
   for (var i = arr.length - 1; i >= 0; i--) {
 
     const parsedNumber = parseInt(arr[i]);
@@ -39,14 +39,16 @@ lines?.forEach((line, index) => {
       break;
     }
 }
+
+//create a valid number from the two numbers
 const firstNumberStr = firstNumber?.toString() || '';
 
 const lastNumberStr = lastNumber?.toString() || '';
 
 const concatenatedString = firstNumberStr + lastNumberStr;
+
+//add valid nubmer to the accumulator variable
 sumOfLine += parseInt(concatenatedString);
-//console.log(firstNumber);
-//console.log(lastNumber);
 
 
 });
