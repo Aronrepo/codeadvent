baed16e9eac61ab4a45b4d73361b2af9d2db4b85
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2024-01-17 12:21:24 +0100

    add comments

--- src/02/02.ts
+++ src/02/02.ts
@@ -1,59 +1,56 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-let fileContent: string | undefined; 
+function sumOfTheSets(filePath: string): number {
+  let fileContent: string | undefined;
 
-const filePath = './input2.txt';
+  try {
+    fileContent = fs.readFileSync(filePath, "utf-8"); //read input file
+  } catch (err) {
+    console.error("Error reading the file:", err);
+  }
 
-try {
-  const filePath = './input2.txt';
+  
+  const lines = fileContent?.split(/\r?\n/); //split input file's lines into arrays
 
-  fileContent = fs.readFileSync(filePath, 'utf-8');
+  let counter: number = 0;
+  //Iterate over the lines
+  lines?.forEach((line) => {
+    let possible: boolean = true;
+    let actualGameNumber: number = parseInt(line.split(":")[0].split(" ")[1]);
 
-} catch (err) {
-  console.error('Error reading the file:', err);
-}
-
-//only 12 red cubes, 13 green cubes, and 14 blue cubes
-const lines = fileContent?.split(/\r?\n/);
-
-
-let counter : number = 0;
-
-lines?.forEach((line) => {
-    let possible : boolean = true;
-    let actualGameNumber : number = parseInt(line.split(':')[0].split(' ')[1]);
-
-    let actualColors : string = line.split(':')[1].trim();
-    
-    let sets : string[] = actualColors.split(';');
-    
-    sets.forEach((oneSet) =>  {
-
-        let setArr : String[] = oneSet.split(',').map(a => a.trim());
-        setArr.forEach((cube) => {
-            if (cube.split(' ')[1] === 'red' && parseInt(cube.split(' ')[0]) > 12)
-            {
-                possible = false;
-            }
-            if (cube.split(' ')[1] === 'green' && parseInt(cube.split(' ')[0]) > 13)
-            {
-                possible = false;
-            }
-            if (cube.split(' ')[1] === 'blue' && parseInt(cube.split(' ')[0]) > 14)
-            {
-                possible = false;
-            }
-        })
-        
-    })
+    let actualColors: string = line.split(":")[1].trim();
+    //split the lines into sets of colors
+    let sets: string[] = actualColors.split(";");
+    //iterate over the sets
+    sets.forEach((oneSet) => {
+      let setArr: String[] = oneSet.split(",").map((a) => a.trim());//split one set into distinct colors
+      //Iterate over a set and check if none of its elements exceed the number of available colors
+      setArr.forEach((cube) => {
+        if (cube.split(" ")[1] === "red" &&
+            parseInt(cube.split(" ")[0]) > 12
+        ) {
+          possible = false;
+        }
+        if (
+          cube.split(" ")[1] === "green" &&
+          parseInt(cube.split(" ")[0]) > 13
+        ) {
+          possible = false;
+        }
+        if (
+          cube.split(" ")[1] === "blue" &&
+          parseInt(cube.split(" ")[0]) > 14
+        ) {
+          possible = false;
+        }
+      });
+    });
     if (possible) {
-        counter += actualGameNumber;
+      counter += actualGameNumber;
     }
-})
+  });
+  return counter;
+}
+console.log(sumOfTheSets("./input2.txt"));
 
-console.log(counter);
-
-
-
-
-
+export default sumOfTheSets;
