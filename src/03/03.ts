2a759dc75a91f3149062f25acc293695e8f5e160
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2024-01-23 12:19:40 +0100

    Change file to be tested easily

--- src/03/03.ts
+++ src/03/03.ts
@@ -1,223 +1,256 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-let fileContent: string | undefined; 
+function sumOfAllParts(filePath: string): number {
+  let fileContent: string | undefined;
 
-const filePath = './input3.txt';
+  try {
+    fileContent = fs.readFileSync(filePath, "utf-8");
+  } catch (err) {
+    console.error("Error reading the file:", err);
+  }
 
-try {
-  const filePath = './input3.txt';
+  const lines = fileContent?.split(/\r?\n/);
 
-  fileContent = fs.readFileSync(filePath, 'utf-8');
+  let sum: number = 0;
 
-} catch (err) {
-  console.error('Error reading the file:', err);
+  lines?.forEach((line, lineIndex) => {
+    let lineArr: string[] = line.split("");
+    lineArr.forEach((char, charIndex) => {
+      if (!/[0-9.]/.test(char) && lineIndex > 0) {
+        sum += searchAround(lines, lineIndex, charIndex);
+      }
+    });
+  });
+
+  return sum;
+
 }
 
-const lines = fileContent?.split(/\r?\n/);
+  function searchAround(
+    lines: string[] | undefined,
+    lineIndex: number,
+    charIndex: number
+  ): number {
+    let searchSum: number = 0;
+    let searchedChars: string = "";
 
-let sum : number = 0;
-
-lines?.forEach((line, lineIndex) => {
-
-    let lineArr : string[] = line.split('');
-    lineArr.forEach((char, charIndex) => {
-        if(!/[0-9.]/.test(char) && lineIndex > 0) {
-            
-            sum += searchAround(lines, lineIndex, charIndex)
-            
+    if (lines) {
+      for (let i = 1; i <= 3; i++) {
+        let actualLine = lines[lineIndex].split("");
+        if (/[0-9]/.test(actualLine[charIndex - i])) {
+          searchedChars = actualLine[charIndex - i] + searchedChars;
+        } else {
+          break;
         }
-        
-    })
+      }
 
-})
+      if (searchedChars !== "") {
+        searchSum += parseInt(searchedChars);
+      }
 
-console.log(sum);
+      searchedChars = "";
 
-
-function searchAround(lines: string[] | undefined, lineIndex : number, charIndex : number) : number {
-    let searchSum : number = 0;
-    let searchedChars : string = '';
-
-    if(lines) {
-        
-        for(let i = 1; i <=3; i++) {
-            let actualLine = lines[lineIndex].split('');
-            if (/[0-9]/.test(actualLine[charIndex - i])) {
-                searchedChars = actualLine[charIndex - i] + searchedChars
-                
-            } else {
-                break;
-            }
-            
+      for (let i = 1; i <= 3; i++) {
+        let actualLine = lines[lineIndex].split("");
+        if (/[0-9]/.test(actualLine[charIndex + i])) {
+          searchedChars = searchedChars + actualLine[charIndex + i];
+        } else {
+          break;
         }
+      }
 
-        if(searchedChars !== '') {
-            searchSum += parseInt(searchedChars);
+      if (searchedChars !== "") {
+        searchSum += parseInt(searchedChars);
+      }
+      searchedChars = "";
+
+      let previousLine = lines[lineIndex - 1].split("");
+
+      if (
+        /[0-9]/.test(previousLine[charIndex]) &&
+        /[0-9]/.test(previousLine[charIndex - 1]) &&
+        /[0-9]/.test(previousLine[charIndex + 1])
+      ) {
+        searchedChars =
+          searchedChars +
+          previousLine[charIndex - 1] +
+          previousLine[charIndex] +
+          previousLine[charIndex + 1];
+      } else if (
+        /[0-9]/.test(previousLine[charIndex]) &&
+        previousLine[charIndex - 1] === "." &&
+        previousLine[charIndex + 1] === "."
+      ) {
+        searchedChars = previousLine[charIndex];
+      } else if (
+        /[0-9]/.test(previousLine[charIndex]) &&
+        previousLine[charIndex - 1] === "." &&
+        /[0-9]/.test(previousLine[charIndex + 1])
+      ) {
+        searchedChars = previousLine[charIndex] + previousLine[charIndex + 1];
+        if (/[0-9]/.test(previousLine[charIndex + 2])) {
+          searchedChars += previousLine[charIndex + 2];
         }
-        
-        console.log(searchedChars);
-        
-
-        searchedChars = '';
-
-        for(let i = 1; i <=3; i++) {
-            let actualLine = lines[lineIndex].split('');
-            if (/[0-9]/.test(actualLine[charIndex + i])) {
-                searchedChars = searchedChars + actualLine[charIndex + i];
-                
-            } else {
-                break;
-            }
-            
+      } else if (
+        /[0-9]/.test(previousLine[charIndex]) &&
+        previousLine[charIndex + 1] === "." &&
+        /[0-9]/.test(previousLine[charIndex - 1])
+      ) {
+        searchedChars = previousLine[charIndex - 1] + previousLine[charIndex];
+        if (/[0-9]/.test(previousLine[charIndex - 2])) {
+          searchedChars = previousLine[charIndex - 2] + searchedChars;
         }
-
-        if(searchedChars !== '') {
-            searchSum += parseInt(searchedChars);
-        } 
-        console.log(searchedChars);
-        searchedChars = '';
-
-        let previousLine = lines[lineIndex - 1].split('');
-
-        if(/[0-9]/.test(previousLine[charIndex]) && /[0-9]/.test(previousLine[charIndex - 1]) && /[0-9]/.test(previousLine[charIndex + 1])) {
-            searchedChars = searchedChars + previousLine[charIndex - 1] + previousLine[charIndex] + previousLine[charIndex + 1];
-        } else if (/[0-9]/.test(previousLine[charIndex]) && previousLine[charIndex - 1] === '.' && previousLine[charIndex + 1] === '.') {
-            searchedChars = previousLine[charIndex];
-        } else if (/[0-9]/.test(previousLine[charIndex]) && previousLine[charIndex - 1] === '.' && /[0-9]/.test(previousLine[charIndex + 1])) {
-            searchedChars = previousLine[charIndex] + previousLine[charIndex + 1];
-            if(/[0-9]/.test(previousLine[charIndex + 2])) {searchedChars += previousLine[charIndex + 2]}
-        } else if (/[0-9]/.test(previousLine[charIndex]) && previousLine[charIndex + 1] === '.' && /[0-9]/.test(previousLine[charIndex - 1])) {
-            searchedChars = previousLine[charIndex - 1] + previousLine[charIndex];
-            if(/[0-9]/.test(previousLine[charIndex - 2])) {searchedChars = previousLine[charIndex - 2] + searchedChars}
-        } else if (previousLine[charIndex + 1] === '.'){
-            let i:number = 1;
-            while(/[0-9]/.test(previousLine[charIndex - i]) && i <= 3) {
-                searchedChars = previousLine[charIndex - i] + searchedChars;
-            i++;
-            }
-            i = 1;
-            while(/[0-9]/.test(previousLine[charIndex + i]) && i <= 3) {
-                searchedChars = searchedChars + previousLine[charIndex + i];
-            i++;
-            }
-
-        } else if (previousLine[charIndex - 1] === '.'){
-            let i:number = 1;
-            while(/[0-9]/.test(previousLine[charIndex + i]) && i <= 3) {
-                searchedChars = searchedChars + previousLine[charIndex + i];
-            i++;
-            }
-            i = 1;
-            while(/[0-9]/.test(previousLine[charIndex - i]) && i <= 3) {
-                searchedChars = previousLine[charIndex - i] + searchedChars;
-            i++;
-            }
+      } else if (previousLine[charIndex + 1] === ".") {
+        let i: number = 1;
+        while (/[0-9]/.test(previousLine[charIndex - i]) && i <= 3) {
+          searchedChars = previousLine[charIndex - i] + searchedChars;
+          i++;
         }
+        i = 1;
+        while (/[0-9]/.test(previousLine[charIndex + i]) && i <= 3) {
+          searchedChars = searchedChars + previousLine[charIndex + i];
+          i++;
+        }
+      } else if (previousLine[charIndex - 1] === ".") {
+        let i: number = 1;
+        while (/[0-9]/.test(previousLine[charIndex + i]) && i <= 3) {
+          searchedChars = searchedChars + previousLine[charIndex + i];
+          i++;
+        }
+        i = 1;
+        while (/[0-9]/.test(previousLine[charIndex - i]) && i <= 3) {
+          searchedChars = previousLine[charIndex - i] + searchedChars;
+          i++;
+        }
+      }
 
-        if(searchedChars !== '') {
-            searchSum += parseInt(searchedChars);
-        } 
+      if (searchedChars !== "") {
+        searchSum += parseInt(searchedChars);
+      }
 
-        if(previousLine[charIndex] === '.' && /[0-9]/.test(previousLine[charIndex - 1]) && /[0-9]/.test(previousLine[charIndex + 1]) ) {
-            
-            let i:number = 1;
+      if (
+        previousLine[charIndex] === "." &&
+        /[0-9]/.test(previousLine[charIndex - 1]) &&
+        /[0-9]/.test(previousLine[charIndex + 1])
+      ) {
+        let i: number = 1;
 
-            while(/[0-9]/.test(previousLine[charIndex - i]) && i <= 3) {
-                searchedChars = previousLine[charIndex - i] + searchedChars;
-            i++;
-            }
-            if(searchedChars !== '') {
-                searchSum += parseInt(searchedChars);
-            } 
-            console.log(searchedChars);
-            
-            searchedChars = ''
-            i = 1;
-            while(/[0-9]/.test(previousLine[charIndex + i]) && i <= 3) {
-                searchedChars = searchedChars + previousLine[charIndex + i];
-            i++;
-            }
-            if(searchedChars !== '') {
-                searchSum += parseInt(searchedChars);
-            } 
-            console.log(searchedChars);
-            
+        while (/[0-9]/.test(previousLine[charIndex - i]) && i <= 3) {
+          searchedChars = previousLine[charIndex - i] + searchedChars;
+          i++;
+        }
+        if (searchedChars !== "") {
+          searchSum += parseInt(searchedChars);
         }
 
+        searchedChars = "";
+        i = 1;
+        while (/[0-9]/.test(previousLine[charIndex + i]) && i <= 3) {
+          searchedChars = searchedChars + previousLine[charIndex + i];
+          i++;
+        }
+        if (searchedChars !== "") {
+          searchSum += parseInt(searchedChars);
+        }
+      }
 
-        searchedChars = ''
-        
-        let nextLine = lines[lineIndex + 1].split('');
+      searchedChars = "";
 
-        if(/[0-9]/.test(nextLine[charIndex]) && /[0-9]/.test(nextLine[charIndex - 1]) && /[0-9]/.test(nextLine[charIndex + 1])) {
-            searchedChars = searchedChars + nextLine[charIndex - 1] + nextLine[charIndex] + nextLine[charIndex + 1];
-        } else if (/[0-9]/.test(nextLine[charIndex]) && nextLine[charIndex - 1] === '.' && nextLine[charIndex + 1] === '.') {
-            searchedChars = nextLine[charIndex];
-        } else if (/[0-9]/.test(nextLine[charIndex]) && nextLine[charIndex - 1] === '.' && /[0-9]/.test(nextLine[charIndex + 1])) {
-            searchedChars = nextLine[charIndex] + nextLine[charIndex + 1];
-            if(/[0-9]/.test(nextLine[charIndex + 2])) {searchedChars += nextLine[charIndex + 2]}
-        } else if (/[0-9]/.test(nextLine[charIndex]) && nextLine[charIndex + 1] === '.' && /[0-9]/.test(nextLine[charIndex - 1])) {
-            searchedChars = nextLine[charIndex - 1] + nextLine[charIndex];
-            if(/[0-9]/.test(nextLine[charIndex - 2])) {searchedChars = nextLine[charIndex - 2] + searchedChars}
-        } else if (nextLine[charIndex + 1] === '.'){
-            let i:number = 1;
-            while(/[0-9]/.test(nextLine[charIndex - i]) && i <= 3) {
-                searchedChars = nextLine[charIndex - i] + searchedChars;
-            i++;
-            }
-            i = 1;
-            while(/[0-9]/.test(nextLine[charIndex + i]) && i <= 3) {
-                searchedChars = searchedChars + nextLine[charIndex + i];
-            i++;
-            }
+      let nextLine = lines[lineIndex + 1].split("");
+      
+      if (
+        /[0-9]/.test(nextLine[charIndex]) &&
+        /[0-9]/.test(nextLine[charIndex - 1]) &&
+        /[0-9]/.test(nextLine[charIndex + 1])
+      ) {
+        searchedChars =
+          searchedChars +
+          nextLine[charIndex - 1] +
+          nextLine[charIndex] +
+          nextLine[charIndex + 1];
+      } else if (
+        /[0-9]/.test(nextLine[charIndex]) &&
+        nextLine[charIndex - 1] === "." &&
+        nextLine[charIndex + 1] === "."
+      ) {
+        searchedChars = nextLine[charIndex];
+      } else if (
+        /[0-9]/.test(nextLine[charIndex]) &&
+        nextLine[charIndex - 1] === "." &&
+        /[0-9]/.test(nextLine[charIndex + 1])
+      ) {
+        searchedChars = nextLine[charIndex] + nextLine[charIndex + 1];
+        if (/[0-9]/.test(nextLine[charIndex + 2])) {
+          searchedChars += nextLine[charIndex + 2];
+        }
+      } else if (
+        /[0-9]/.test(nextLine[charIndex]) &&
+        nextLine[charIndex + 1] === "." &&
+        /[0-9]/.test(nextLine[charIndex - 1])
+      ) {
+        searchedChars = nextLine[charIndex - 1] + nextLine[charIndex];
+        if (/[0-9]/.test(nextLine[charIndex - 2])) {
+          searchedChars = nextLine[charIndex - 2] + searchedChars;
+        }
+      } else if (nextLine[charIndex + 1] === ".") {
+        let i: number = 1;
+        while (/[0-9]/.test(nextLine[charIndex - i]) && i <= 3) {
+          searchedChars = nextLine[charIndex - i] + searchedChars;
+          i++;
+        }
+        i = 1;
+        while (/[0-9]/.test(nextLine[charIndex + i]) && i <= 3) {
+          searchedChars = searchedChars + nextLine[charIndex + i];
+          i++;
+        }
+      } else if (nextLine[charIndex - 1] === ".") {
+        let i: number = 1;
+        while (/[0-9]/.test(nextLine[charIndex + i]) && i <= 3) {
+          searchedChars = searchedChars + nextLine[charIndex + i];
+          i++;
+        }
+        i = 1;
+        while (/[0-9]/.test(nextLine[charIndex - i]) && i <= 3) {
+          searchedChars = nextLine[charIndex - i] + searchedChars;
+          i++;
+        }
+      }
 
-        } else if (nextLine[charIndex - 1] === '.'){
-            let i:number = 1;
-            while(/[0-9]/.test(nextLine[charIndex + i]) && i <= 3) {
-                searchedChars = searchedChars + nextLine[charIndex + i];
-            i++;
-            }
-            i = 1;
-            while(/[0-9]/.test(nextLine[charIndex - i]) && i <= 3) {
-                searchedChars = nextLine[charIndex - i] + searchedChars;
-            i++;
-            }
+      if (searchedChars !== "") {
+        searchSum += parseInt(searchedChars);
+      }
+
+      if (
+        nextLine[charIndex] === "." &&
+        /[0-9]/.test(nextLine[charIndex - 1]) &&
+        /[0-9]/.test(nextLine[charIndex + 1])
+      ) {
+        let i: number = 1;
+
+        while (/[0-9]/.test(nextLine[charIndex - i]) && i <= 3) {
+          searchedChars = nextLine[charIndex - i] + searchedChars;
+          i++;
+        }
+        if (searchedChars !== "") {
+          searchSum += parseInt(searchedChars);
         }
 
-        if(searchedChars !== '') {
-            searchSum += parseInt(searchedChars);
-        } 
-        
-        if(nextLine[charIndex] === '.' && /[0-9]/.test(nextLine[charIndex - 1]) && /[0-9]/.test(nextLine[charIndex + 1]) ) {
-            
-            let i:number = 1;
-
-            while(/[0-9]/.test(nextLine[charIndex - i]) && i <= 3) {
-                searchedChars = nextLine[charIndex - i] + searchedChars;
-            i++;
-            }
-            if(searchedChars !== '') {
-                searchSum += parseInt(searchedChars);
-            } 
-            console.log(searchedChars);
-            
-            searchedChars = ''
-            i = 1;
-            while(/[0-9]/.test(nextLine[charIndex + i]) && i <= 3) {
-                searchedChars = searchedChars + nextLine[charIndex + i];
-            i++;
-            }
-            if(searchedChars !== '') {
-                searchSum += parseInt(searchedChars);
-            } 
-            console.log(searchedChars);
-            
+        searchedChars = "";
+        i = 1;
+        while (/[0-9]/.test(nextLine[charIndex + i]) && i <= 3) {
+          searchedChars = searchedChars + nextLine[charIndex + i];
+          i++;
         }
-
-
-
-        
+        if (searchedChars !== "") {
+          searchSum += parseInt(searchedChars);
+        }
+      }
     }
 
     return searchSum;
-}
+  }
+
+
+console.log(sumOfAllParts("./input3.txt"));
+
+export { sumOfAllParts, searchAround };
