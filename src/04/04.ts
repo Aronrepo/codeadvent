af806124fed991c48c8e163ec76a04e51e2acafb
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2024-02-01 09:50:00 +0100

    add comments

--- src/04/04.ts
+++ src/04/04.ts
@@ -7,31 +7,26 @@ const filePath = './input4.txt';
 try {
   const filePath = './input4.txt';
 
-  fileContent = fs.readFileSync(filePath, 'utf-8');
+  fileContent = fs.readFileSync(filePath, 'utf-8');//read input file
 
 } catch (err) {
   console.error('Error reading the file:', err);
 }
 
-const lines = fileContent?.split(/\r?\n/);
+const lines = fileContent?.split(/\r?\n/);//split input file's lines into arrays
 
 let sumPoints = 0;
-
+//Iterate over the lines
 lines?.forEach(line => {
-    let winnerNumbersString:string = line.split("|")[0].split(':')[1].trim().replace(/\s+/g, ' ');
-    let ownNumbersString:string = line.split("|")[1].trim().replace(/\s+/g, ' ');
+    let winnerNumbersString:string = line.split("|")[0].split(':')[1].trim().replace(/\s+/g, ' ');//get winner numbers as string 
+    let ownNumbersString:string = line.split("|")[1].trim().replace(/\s+/g, ' ');//get own numbers as String
 
-    //console.log(winnerNumbers.split(':')[1].trim());
+    let winnerNumbers:string[] = winnerNumbersString.split(' ');//put winner numbers into array
+    let ownNumbers:string[] = ownNumbersString.split(' ');//put own numbers into array
 
-    let winnerNumbers:string[] = winnerNumbersString.split(' ');
-    let ownNumbers:string[] = ownNumbersString.split(' ');
-
-    //console.log(winnerNumbers);
-    //console.log(ownNumbers);
-
-    let matches = ownNumbers.filter((item) => winnerNumbers.includes(item));
+    let matches = ownNumbers.filter((item) => winnerNumbers.includes(item)); //get matches
     if (matches.length !== 0) {
-        sumPoints += Math.pow(2, matches.length - 1);
+        sumPoints += Math.pow(2, matches.length - 1);//double the point for every match
         
     }
     
