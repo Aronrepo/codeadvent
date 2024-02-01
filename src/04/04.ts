173754822876583f4d4325d7cd43e056c15366c3
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2024-02-01 10:33:36 +0100

    place the logic into a function

--- src/04/04.ts
+++ src/04/04.ts
@@ -1,37 +1,40 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-let fileContent: string | undefined; 
+function sumOfTotalPoints(filePath: string): number {
+  let fileContent: string | undefined;
 
-const filePath = './input4.txt';
+  try {
+    fileContent = fs.readFileSync(filePath, "utf-8"); //read input file
+  } catch (err) {
+    console.error("Error reading the file:", err);
+  }
 
-try {
-  const filePath = './input4.txt';
+  const lines = fileContent?.split(/\r?\n/); //split input file's lines into arrays
 
-  fileContent = fs.readFileSync(filePath, 'utf-8');//read input file
+  let sumPoints = 0;
+  //Iterate over the lines
+  lines?.forEach((line) => {
+    let winnerNumbersString: string = line
+      .split("|")[0]
+      .split(":")[1]
+      .trim()
+      .replace(/\s+/g, " "); //get winner numbers as string
+    let ownNumbersString: string = line
+      .split("|")[1]
+      .trim()
+      .replace(/\s+/g, " "); //get own numbers as String
 
-} catch (err) {
-  console.error('Error reading the file:', err);
-}
-
-const lines = fileContent?.split(/\r?\n/);//split input file's lines into arrays
-
-let sumPoints = 0;
-//Iterate over the lines
-lines?.forEach(line => {
-    let winnerNumbersString:string = line.split("|")[0].split(':')[1].trim().replace(/\s+/g, ' ');//get winner numbers as string 
-    let ownNumbersString:string = line.split("|")[1].trim().replace(/\s+/g, ' ');//get own numbers as String
-
-    let winnerNumbers:string[] = winnerNumbersString.split(' ');//put winner numbers into array
-    let ownNumbers:string[] = ownNumbersString.split(' ');//put own numbers into array
+    let winnerNumbers: string[] = winnerNumbersString.split(" "); //put winner numbers into array
+    let ownNumbers: string[] = ownNumbersString.split(" "); //put own numbers into array
 
     let matches = ownNumbers.filter((item) => winnerNumbers.includes(item)); //get matches
     if (matches.length !== 0) {
-        sumPoints += Math.pow(2, matches.length - 1);//double the point for every match
-        
+      sumPoints += Math.pow(2, matches.length - 1); //double the point for every match
     }
-    
-})
+  });
 
-console.log(sumPoints);
+  return sumPoints;
+}
+console.log(sumOfTotalPoints("./input4.txt"));
 
-
+export default sumOfTotalPoints;
